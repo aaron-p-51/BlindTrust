f5a3d5c30e1ae74514ae3c3f69a7fb4954c453f7
Aaron <petrekaj@gmail.com>
2023-02-07 13:47:23 -0800

    Added BlindPlayerPawn source files

---
+++ Source/WoLDTheCorridor/Private/ZBlindPlayerPawn.cpp
@@ -0,0 +1,100 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZBlindPlayerPawn.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+AZBlindPlayerPawn::AZBlindPlayerPawn()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+}
+
+// Called when the game starts or when spawned
+void AZBlindPlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZBlindPlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+	float Speed = CalculateSpeed();
+	if (Speed == 0.f)
+	{
+		bRotateRootBone = true;
+		FRotator CurrentAimRotation = FRotator(0.f, GetBaseAimRotation().Yaw, 0.f);
+		FRotator DeltaAimRotation = UKismetMathLibrary::NormalizedDeltaRotator(CurrentAimRotation, StartingAimRotation);
+		AimOffset_Yaw = DeltaAimRotation.Yaw;
+
+		if (TurningInPlace == ETurningInPlace::ETIP_NotTurning)
+		{
+			InterpAimOffset_Yaw = AimOffset_Yaw;
+		}
+
+		//bUseControllerRotationYaw = false;
+
+		TurnInPlace(DeltaTime);
+	}
+
+	if (Speed > 0.f)
+	{
+		bRotateRootBone = false;
+		StartingAimRotation = FRotator(0.f, GetBaseAimRotation().Yaw, 0.f);
+		AimOffset_Yaw = 0.f;
+		//bUseControllerRotationYaw = true;
+		TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+		
+	}
+
+}
+
+
+// Called to bind functionality to input
+void AZBlindPlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+
+float AZBlindPlayerPawn::CalculateSpeed() const
+{
+	FVector Velocity = GetVelocity();
+	Velocity.Z = 0.f;
+
+	return Velocity.Size();
+}
+
+
+void AZBlindPlayerPawn::TurnInPlace(float DeltaTime)
+{
+	if (AimOffset_Yaw > 40.f)
+	{
+		TurningInPlace = ETurningInPlace::ETIP_Right;
+	}
+	else if (AimOffset_Yaw < -40.f)
+	{
+		TurningInPlace = ETurningInPlace::ETIP_Left;
+	}
+
+	if (TurningInPlace != ETurningInPlace::ETIP_NotTurning)
+	{
+		InterpAimOffset_Yaw = FMath::FInterpTo(InterpAimOffset_Yaw, 0.f, DeltaTime, 4.f);
+		AimOffset_Yaw = InterpAimOffset_Yaw;
+		if (FMath::Abs(AimOffset_Yaw) < 15.f)
+		{
+			TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+			StartingAimRotation = FRotator(0.f, GetBaseAimRotation().Yaw, 0.f);
+		}
+	}
+}
+
---
+++ Source/WoLDTheCorridor/Public/ZBlindPlayerPawn.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ZombieChaseTypes.h"
+#include "ZBlindPlayerPawn.generated.h"
+
+UCLASS()
+class WOLDTHECORRIDOR_API AZBlindPlayerPawn : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AZBlindPlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bRotateRootBone;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator StartingAimRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AimOffset_Yaw;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float InterpAimOffset_Yaw;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ETurningInPlace TurningInPlace;
+
+
+
+
+private:
+
+	float CalculateSpeed() const;
+
+	void TurnInPlace(float DeltaTime);
+
+};
